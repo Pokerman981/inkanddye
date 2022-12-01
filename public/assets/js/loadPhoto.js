d9958f25cf6a88f97a6126d35c3900cedf18da2d
troyg <troy.gidney@gmail.com>
2022-12-01 00:17:45 -0700

    Adding modularity for reused elements. S/C

--- public/assets/js/loadPhoto.js
+++ public/assets/js/loadPhoto.js
@@ -1,6 +1,34 @@
-function onLoad() {
-    var element = document.getElementById("galleryStart");
+function loadPhoto() {
+    console.log(firebase);
+    const storage = firebase.app().storage();
+    const storageList = storage.ref().child("images/").listAll();
 
-    
-    
+    const gallery = document.querySelector("#galleryStart");
+
+    storageList.then((e) => {
+      for (let name of e.items) {
+        storage.ref().child(`images/${name.name}`).getDownloadURL().then((url) => {
+
+          // TODO Perform sanitization
+
+        let text = `          
+        <div class="col-xl-3 col-lg-4 col-md-6">
+          <div class="gallery-item h-100">
+            <img src="${url}" class="img-fluid" alt="">
+            <div class="gallery-links d-flex align-items-center justify-content-center">
+              <a href="${url}" title="Gallery 1" class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
+              <a href="${name.name}.html" class="details-link"><i class="bi bi-link-45deg"></i></a>
+            </div>
+          </div>
+        </div><!-- End Gallery Item -->`
+
+        gallery.innerHTML = gallery.innerHTML + text;
+
+        console.log(name.name);
+
+      });
+      }
+    });
 }
+
+loadPhoto();
